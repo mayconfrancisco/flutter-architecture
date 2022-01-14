0098187e8da6e3b00700bff025c095218eb5f42e
Maycon Francisco <mayconjf@gmail.com>
2022-01-14 08:50:38 -0300

    garantindo que HttpAdapter chame post sem body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,8 +20,9 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
+    final jsonBody = body != null ? jsonEncode(body) : null;
 
-    client.post(url, headers: headers, body: jsonEncode(body));
+    client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -49,6 +50,12 @@ void main() {
             'accept': 'application/json',
           },
           body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(any, headers: anyNamed('headers')));
     });
   });
 }
