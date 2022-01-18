dad34ffd536f89f19928829f507f595035c52d3a
Maycon Francisco <mayconjf@gmail.com>
2022-01-18 08:18:23 -0300

    movido HttpAdapter para um arquivo de producao

--- requirements/http/http_responses.md
+++ requirements/http/http_responses.md
@@ -5,7 +5,7 @@
 2. ✅ Passar nos headers o content type JSON
 3. ✅ chamar request com o body correto
 4. ✅ 0k - 200 e resposta com dados
-5. No content - 204 e resposta sem dados
+5. ✅ No content - 204 e resposta sem dados
 
 > ## Erros
 1. Bad request - 400
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,35 +1,8 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
+import 'package:flutter_architecture/infra/http/http.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-
-class HttpAdapter {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-
-    final response = await client.post(url, headers: headers, body: jsonBody);
-
-    if (response.statusCode == 204) {
-      return null;
-    }
-    return response.body.isNotEmpty ? jsonDecode(response.body) : null;
-  }
-}
 
 class ClientSpy extends Mock implements Client {}
 
